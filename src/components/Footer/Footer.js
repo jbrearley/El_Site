05ba727c263036ad95fae2df77287d7e6a3ac2d4
Andrew <andrewpeliza@hotmail.com>
2020-01-27 21:05:18 +0000

    Added footer & reusable css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const Footer = () => <p>Footer</p>;
+import './Footer.scss';
+
+const Footer = () => (
+  <section className='section-dk-blue'>
+    <div className='wrapper flex-row footer-container'>
+      <article>
+        <p>Footer left</p>
+      </article>
+      <article>
+        <p>Footer right</p>
+      </article>
+    </div>
+  </section>
+);
 
 export default Footer;
