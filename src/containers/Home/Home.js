e878de207b112786a3653cad88b00c070dfa57b9
Andrew <andrewpeliza@hotmail.com>
2020-01-30 20:15:18 +0000

    Added helpers.css to be used as helper css classes.

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import './Home.scss';
+
 function Home() {
-  return <p>home</p>;
+  return (
+    <section className='section-grey'>
+      <div className='wrapper'>
+        <article>
+          <h2>Home</h2>
+          <p>The home page info</p>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
