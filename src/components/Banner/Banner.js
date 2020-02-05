56027649172f98de904dad715307c287b01600dd
Andrew <andrewpeliza@hotmail.com>
2020-02-05 11:48:07 +0000

    Banner fixes, also implemented a timeline for the experience page.

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,9 +4,55 @@ import './Banner.scss';
 
 const Banner = () => {
   return (
-    <section>
+    <section className='section-banner'>
       <div className='wrapper flex to-end'>
-        <p>hi</p>
+        <ul className='banner-ul'>
+          <li>
+            <a
+              href='https://www.linkedin.com/in/andrewpeliza'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i
+                className='fab fa-linkedin fa-lg banner-fa-res'
+                style={{ color: '#1DA1F1' }}
+              ></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://twitter.com/APeliza'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i
+                className='fab fa-twitter fa-lg'
+                style={{ color: '#1DA1F1' }}
+              ></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://github.com/ElAndy94'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='fab fa-github fa-lg'></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://stackoverflow.com/users/4920343/andrew?tab=profile'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i
+                className='fab fa-stack-overflow fa-lg'
+                style={{ color: '#FF9900' }}
+              ></i>
+            </a>
+          </li>
+        </ul>
       </div>
     </section>
   );
--- src/containers/Experience/Experience.js
+++ src/containers/Experience/Experience.js
@@ -1,11 +1,119 @@
 import React from 'react';
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
 
 const Experience = () => (
   <section className='section'>
     <div className='wrapper'>
       <article>
-        <h2>Experience</h2>
-        <p>The experience page info</p>
+        <Timeline lineColor={'#ddd'}>
+          <TimelineItem
+            key='001'
+            dateText='March 2019 – Present'
+            style={{ color: '#e86971' }}
+          >
+            <h3 className='padding-bottom-sm'>
+              Frontend Developer, Think Money Group
+            </h3>
+            <p>
+              Est incididunt sint eu minim dolore mollit velit velit commodo ex
+              nulla exercitation. Veniam velit adipisicing anim excepteur
+              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
+              officia irure reprehenderit laborum fugiat dolore in elit.
+              Adipisicing do qui duis Lorem est.
+            </p>
+            <p>
+              Est incididunt sint eu minim dolore mollit velit velit commodo ex
+              nulla exercitation. Veniam velit adipisicing anim excepteur
+              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
+              officia irure reprehenderit laborum fugiat dolore in elit.
+              Adipisicing do qui duis Lorem est.
+            </p>
+            <p>
+              Est incididunt sint eu minim dolore mollit velit velit commodo ex
+              nulla exercitation. Veniam velit adipisicing anim excepteur
+              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
+              officia irure reprehenderit laborum fugiat dolore in elit.
+              Adipisicing do qui duis Lorem est.
+            </p>
+          </TimelineItem>
+          <TimelineItem
+            key='002'
+            dateText='04/2009 – 11/2010'
+            dateInnerStyle={{ background: '#61b8ff', color: '#000' }}
+            bodyContainerStyle={{
+              background: '#ddd',
+              padding: '20px',
+              borderRadius: '8px',
+              boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)'
+            }}
+          >
+            <h3 className='padding-bottom-sm' style={{ color: '#61b8ff' }}>
+              Title, Company
+            </h3>
+            <p>
+              Est incididunt sint eu minim dolore mollit velit velit commodo ex
+              nulla exercitation. Veniam velit adipisicing anim excepteur
+              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
+              officia irure reprehenderit laborum fugiat dolore in elit.
+              Adipisicing do qui duis Lorem est.
+            </p>
+            <p>
+              Est incididunt sint eu minim dolore mollit velit velit commodo ex
+              nulla exercitation. Veniam velit adipisicing anim excepteur
+              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
+              officia irure reprehenderit laborum fugiat dolore in elit.
+              Adipisicing do qui duis Lorem est.
+            </p>
+          </TimelineItem>
+          <TimelineItem
+            key='003'
+            dateText='08/2008 – 11/2008'
+            dateInnerStyle={{ background: 'rgb(150, 150, 150)' }}
+          >
+            <h3 className='padding-bottom-sm'>Title, Company</h3>
+            <p>
+              Est incididunt sint eu minim dolore mollit velit velit commodo ex
+              nulla exercitation. Veniam velit adipisicing anim excepteur
+              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
+              officia irure reprehenderit laborum fugiat dolore in elit.
+              Adipisicing do qui duis Lorem est.
+            </p>
+            <p>
+              Est incididunt sint eu minim dolore mollit velit velit commodo ex
+              nulla exercitation. Veniam velit adipisicing anim excepteur
+              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
+              officia irure reprehenderit laborum fugiat dolore in elit.
+              Adipisicing do qui duis Lorem est.
+            </p>
+          </TimelineItem>
+          <TimelineItem
+            key='004'
+            dateText='08/2008 – 11/2008'
+            dateInnerStyle={{ background: '#76bb7f' }}
+            bodyContainerStyle={{
+              background: '#ddd',
+              padding: '20px',
+              borderRadius: '8px',
+              boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)'
+            }}
+          >
+            <h3 className='padding-bottom-sm'>Title, Company</h3>
+            <p>
+              Est incididunt sint eu minim dolore mollit velit velit commodo ex
+              nulla exercitation. Veniam velit adipisicing anim excepteur
+              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
+              officia irure reprehenderit laborum fugiat dolore in elit.
+              Adipisicing do qui duis Lorem est.
+            </p>
+            <p>
+              Est incididunt sint eu minim dolore mollit velit velit commodo ex
+              nulla exercitation. Veniam velit adipisicing anim excepteur
+              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
+              officia irure reprehenderit laborum fugiat dolore in elit.
+              Adipisicing do qui duis Lorem est.
+            </p>
+          </TimelineItem>
+        </Timeline>
       </article>
     </div>
   </section>
