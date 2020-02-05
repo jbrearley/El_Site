631de1aef088c6fa7ad862fc68234f129bf41f4d
Andrew <andrewpeliza@hotmail.com>
2020-02-05 12:26:12 +0000

    Added content into Experience page and fixed layout.

--- src/containers/Experience/Experience.js
+++ src/containers/Experience/Experience.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
 
+import './Experience.scss';
+
 const Experience = () => (
   <section className='section'>
     <div className='wrapper'>
-      <article>
+      <article className='timeline-container'>
         <Timeline lineColor={'#ddd'}>
           <TimelineItem
             key='001'
@@ -15,31 +17,35 @@ const Experience = () => (
               Frontend Developer, Think Money Group
             </h3>
             <p>
-              Est incididunt sint eu minim dolore mollit velit velit commodo ex
-              nulla exercitation. Veniam velit adipisicing anim excepteur
-              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
-              officia irure reprehenderit laborum fugiat dolore in elit.
-              Adipisicing do qui duis Lorem est.
+              • Building the front end of web based applications using the
+              latest techniques and technologies (full websites, customer
+              journeys, landing pages, HTML ads).
             </p>
             <p>
-              Est incididunt sint eu minim dolore mollit velit velit commodo ex
-              nulla exercitation. Veniam velit adipisicing anim excepteur
-              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
-              officia irure reprehenderit laborum fugiat dolore in elit.
-              Adipisicing do qui duis Lorem est.
+              • Working in a cross functional team, and attending SCRUMs and
+              project meetings.
             </p>
             <p>
-              Est incididunt sint eu minim dolore mollit velit velit commodo ex
-              nulla exercitation. Veniam velit adipisicing anim excepteur
-              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
-              officia irure reprehenderit laborum fugiat dolore in elit.
-              Adipisicing do qui duis Lorem est.
+              • Liaising with other front-end and back-end developers,
+              designers, SEO team, PPC, testers and implementation staff.
             </p>
+            <p>
+              • Ensuring cross-platform optimisation across required devices.
+            </p>
+            <p>
+              • Interpreting customer requirements to deliver efficient,
+              functional solutions.
+            </p>
+            <p>
+              • Keeping up-to-date with latest technologies and suggest when
+              appropriate to implement.
+            </p>
+            <p>• Assisting in investigating and resolving web/software bugs.</p>
           </TimelineItem>
           <TimelineItem
             key='002'
-            dateText='04/2009 – 11/2010'
-            dateInnerStyle={{ background: '#61b8ff', color: '#000' }}
+            dateText='Jan 2019 – April 2019'
+            dateInnerStyle={{ background: '#61b8ff', color: '#fff' }}
             bodyContainerStyle={{
               background: '#ddd',
               padding: '20px',
@@ -48,47 +54,52 @@ const Experience = () => (
             }}
           >
             <h3 className='padding-bottom-sm' style={{ color: '#61b8ff' }}>
-              Title, Company
+              Lead Frontend Developer, Dyne (Contract)
             </h3>
             <p>
-              Est incididunt sint eu minim dolore mollit velit velit commodo ex
-              nulla exercitation. Veniam velit adipisicing anim excepteur
-              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
-              officia irure reprehenderit laborum fugiat dolore in elit.
-              Adipisicing do qui duis Lorem est.
+              • Using React inc Redux combined with Typescript ( Graphql ) Node.
             </p>
             <p>
-              Est incididunt sint eu minim dolore mollit velit velit commodo ex
-              nulla exercitation. Veniam velit adipisicing anim excepteur
-              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
-              officia irure reprehenderit laborum fugiat dolore in elit.
-              Adipisicing do qui duis Lorem est.
+              • Coordinating with, and implementing handovers from, the design
+              team.
+            </p>
+            <p>• Developing production grade front end web architecture.</p>
+            <p>
+              • Ensuring cross-platform optimisation across required devices.{' '}
+            </p>
+            <p>
+              • Conceptualising and implementing robust QA and testing
+              frameworks.{' '}
+            </p>
+            <p>
+              • Taking ownership of, and managing / seeing through a project
+              from conception to finalisation.{' '}
+            </p>
+            <p>• Meeting both technical and business needs. </p>
+            <p>
+              • Staying abreast of developments in web applications and
+              programming languages.
             </p>
           </TimelineItem>
           <TimelineItem
             key='003'
-            dateText='08/2008 – 11/2008'
+            dateText='Sep 2018 – Nov 2018'
             dateInnerStyle={{ background: 'rgb(150, 150, 150)' }}
           >
-            <h3 className='padding-bottom-sm'>Title, Company</h3>
+            <h3 className='padding-bottom-sm'>
+              FullStack Developer, Eversmart Energy (Contract)
+            </h3>
+            <p>• Developing software in time for release date. </p>
+            <p>• Making sure that the system is ready for lunch day. </p>
             <p>
-              Est incididunt sint eu minim dolore mollit velit velit commodo ex
-              nulla exercitation. Veniam velit adipisicing anim excepteur
-              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
-              officia irure reprehenderit laborum fugiat dolore in elit.
-              Adipisicing do qui duis Lorem est.
+              • Checking that the full website is reponsive on all browsers{' '}
             </p>
-            <p>
-              Est incididunt sint eu minim dolore mollit velit velit commodo ex
-              nulla exercitation. Veniam velit adipisicing anim excepteur
-              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
-              officia irure reprehenderit laborum fugiat dolore in elit.
-              Adipisicing do qui duis Lorem est.
-            </p>
+            <p>• Fixing errors or simplying code. </p>
+            <p>• Ensure consistency throughout.</p>
           </TimelineItem>
           <TimelineItem
             key='004'
-            dateText='08/2008 – 11/2008'
+            dateText='Jun 2018 – Sep 2018'
             dateInnerStyle={{ background: '#76bb7f' }}
             bodyContainerStyle={{
               background: '#ddd',
@@ -97,21 +108,24 @@ const Experience = () => (
               boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)'
             }}
           >
-            <h3 className='padding-bottom-sm'>Title, Company</h3>
+            <h3 className='padding-bottom-sm'>
+              Software Developer, Manchester Metropolitan University (Contract)
+            </h3>
+            <p>• Develop a bespoke application for real-world use.</p>
             <p>
-              Est incididunt sint eu minim dolore mollit velit velit commodo ex
-              nulla exercitation. Veniam velit adipisicing anim excepteur
-              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
-              officia irure reprehenderit laborum fugiat dolore in elit.
-              Adipisicing do qui duis Lorem est.
+              • Research, design, implement and test a custom web application to
+              support the creation of augmented reading lists.
             </p>
             <p>
-              Est incididunt sint eu minim dolore mollit velit velit commodo ex
-              nulla exercitation. Veniam velit adipisicing anim excepteur
-              nostrud magna nostrud aliqua dolor. Sunt aute est duis ut nulla
-              officia irure reprehenderit laborum fugiat dolore in elit.
-              Adipisicing do qui duis Lorem est.
+              • An application which allows tutors to upload documents which can
+              then be annotated with simplifications and explanations where
+              necessary.
             </p>
+            <p>
+              • Participating in ongoing research and making a difference to the
+              learning experience of other students.
+            </p>
+            <p>• Ensure consistency and maximise efficiency.</p>
           </TimelineItem>
         </Timeline>
       </article>
